15a00ba7c2df97369cff68ebe75eb21d22798849
Kevin Keefe <kevin.keefe@IFScientific.com>
2025-02-06 14:30:17 -0600

    clean up

--- software/eth_ctrl.py
+++ software/eth_ctrl.py
@@ -1,11 +1,7 @@
-# interfacing dependcies
-from eth_interface import (eth_interface, EthBadAddr, REG, DEFAULT_PACKET_SIZE, ZYBO_FRQ)
-import os
 import sys
 import time
 
 # PyQt GUI things
-from PyQt5 import QtCore
 from PyQt5.QtWidgets import (QWidget, QPushButton, QCheckBox, QComboBox, QSpinBox, QLabel,
                              QDoubleSpinBox, QProgressBar, QTabWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QStatusBar,
                              QDialog, QDialogButtonBox, QLCDNumber, QFileDialog)
@@ -13,8 +9,7 @@ from PyQt5.QtCore import QProcess, QTimer, pyqtSignal
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QMainWindow, QAction
 
-from qda_reg import QDA_CTRL_REG
-
+from eth_interface import (eth_interface, EthBadAddr, DEFAULT_PACKET_SIZE)
 
 class GUI(QMainWindow):
 
@@ -25,16 +20,14 @@ class GUI(QMainWindow):
 
         # IO interfaces
         self.eth = eth_interface()
+        if not self.eth.isConnected():
+            sys.exit(-1)
+
         self.close_udp.connect(self.eth.finish) # closes udp worker thread
         self.version = self.eth.version
 
         # window setup
         self.setWindowTitle('Ethernet Control')
-
-        # passive triggering
-        self._clock = QTimer()
-        self._clock.timeout.connect(self.trigger)
-        self._lastTrig = -1
 
         # initialize the sub menus
         self._make_menuBar()
@@ -60,12 +53,12 @@ class GUI(QMainWindow):
 
         btn_init = QPushButton()
         btn_init.setText('read ID')
-        btn_init.clicked.connect(self.readID)
+        # btn_init.clicked.connect(self.readID)
         layout.addWidget(btn_init, 0, 0)
 
         btn = QPushButton()
         btn.setText('trigger')
-        btn.clicked.connect(lambda: self.trigger(hard=True))
+        # btn.clicked.connect(lambda: self.trigger(hard=True))
         layout.addWidget(btn, 0, 1)
 
         btn_gtimeout = QPushButton()
@@ -104,174 +97,35 @@ class GUI(QMainWindow):
 
         scan_dac = QPushButton()
         scan_dac.setText('Scan DAC')
-        scan_dac.clicked.connect(self.ScanDAC)
+        # scan_dac.clicked.connect(self.ScanDAC)
         layout.addWidget(scan_dac, 0, 0)
 
         btn = QPushButton()
         btn.setText('Reset DAC')
-        btn.clicked.connect(self.ResetDAC)
+        # btn.clicked.connect(self.ResetDAC)
         layout.addWidget(btn, 0, 1)
 
         dbtn = QPushButton()
         dbtn.setText('Read Debug')
-        dbtn.clicked.connect(self.ReadDebug)
+        # dbtn.clicked.connect(self.ReadDebug)
         layout.addWidget(dbtn, 1, 0)
 
         hIntbtn = QPushButton()
         hIntbtn.setText('Hard Int')
-        hIntbtn.clicked.connect(self.HardInt)
+        # hIntbtn.clicked.connect(self.HardInt)
         layout.addWidget(hIntbtn, 1, 1)
 
         hIntbtn = QPushButton()
         hIntbtn.setText('Reset ASIC')
-        hIntbtn.clicked.connect(self.ResetASIC)
+        # hIntbtn.clicked.connect(self.ResetASIC)
         layout.addWidget(hIntbtn, 2, 0)
 
         self._testPage.setLayout(layout)
         return self._testPage
 
     ############################
-    ## Test specific Commands ##
-    ############################
-    def ScanDAC(self):
-        """
-        Looking for the correct reg load and latch period combinations to set the DAC
-        Per Gang: 16 bit word. 12 lowest are DAC. Bit<12> is used for power on and should be 'on'
-        """
-        # over 2 V spike at 15 load val
-        loadVals = [400]
-        # latchVals = [v for v in range(50, 401, 20)]
-        latchVals = [400]
-
-        ## setting the bits
-        print('scanning dac')
-        p_on = lambda x: ((x & 0x1) << 12)
-        dac_set = lambda x: (x & 0xffff)
-        cap_set = lambda x: ((x & 0x3) << 13)
-        def dac_setting(dac_val, power_on=True, cap=0x3):
-            return p_on(power_on) + dac_set(dac_val) + cap_set(cap)
-
-        def bit_rev(b, rng=0, sz=16):
-            """
-            Reverse the endian-ness of the bit stream if we want to
-            """
-            if not(isinstance(rng, int)) or not (isinstance(sz, int)):
-                return
-            if rng>=int(sz/2):
-                return b
-            else:
-                bp1 = rng
-                bp2 = sz - rng - 1
-                bv1 = b & (1 << bp1)
-                bv2 = b & (1 << bp2)
-                # if bv1 == bv2 there's nothing to do
-                if bv1 and not bv2: # downsize
-                    b = b + (1 << bp2) - (1 << bp1)
-                elif bv2 and not bv1: # upsize
-                    b = b - (1 << bp2) + (1 << bp1)
-                bit_rev(b, rng=rng+1, sz=sz)
-
-        dv = dac_setting(0xaaaa)
-        time.sleep(0.5)
-        self.ResetDAC()
-        self.SetSPILoad(loadVals[0])
-        self.SetSPILatch(latchVals[0])
-        time.sleep(0.5)
-        self.SetDAC(dv)
-
-
-    def SetDAC(self, val):
-        """
-        Update value on the DAC
-        """
-        addr = QDA_CTRL_REG.SPI_DATA.value
-        self.eth.regWrite(addr, val)
-
-    def ResetDAC(self):
-        """
-        Send a high followed by a low pulse to the SPI_SRST port at addr 0x17
-        """
-        addr = QDA_CTRL_REG.SPI_RESET.value
-        val = 1
-        self.eth.regWrite(addr, val)
-
-        val = 0
-        self.eth.regWrite(addr, val)
-
-    def ResetASIC(self):
-        """
-        Send Digital reset
-        """
-        addr = QDA_CTRL_REG.SEND_DEBUG.value
-        val = 3
-        self.eth.regWrite(addr, val)
-
-    def SetSPILoad(self, val):
-        """
-        Configure the SPI load value
-        """
-        addr = QDA_CTRL_REG.SPI_LOAD_PERIOD.value
-        self.eth.regWrite(addr, val)
-
-    def SetSPILatch(self, val):
-        """
-        Configure the SPI latch value
-        """
-        addr = QDA_CTRL_REG.SPI_LATCH_PERIOD.value
-        self.eth.regWrite(addr, val)
-
-    def ReadDebug(self):
-        """
-        Read the Debug Bits
-        """
-        dVals = self.eth.regRead(QDA_CTRL_REG.READ_DEBUG.value)
-        dTxBusy = (dVals & (1 << 0)) > 0
-        dRxError = (dVals & (1 << 1)) > 0
-        dRxBusy = (dVals & (1 << 2)) > 0
-        dExtFifoFull = (dVals & (1 << 3)) > 0
-        dLocFifoFull = (dVals & (1 << 4)) > 0
-        dFsmState = (dVals & (0b111 << 5)) >> 5
-
-        print("status of ASIC: ")
-        if dTxBusy:
-            print("TX BUSY")
-        if dRxError:
-            print("Rx ERROR")
-        if dRxBusy:
-            print("RX BUSY")
-        if dExtFifoFull:
-            print("Ext FIFO Full")
-        if dLocFifoFull:
-            print("Loc FIFO Full")
-        print("FSM State: ", dFsmState)
-
-    def HardInt(self):
-        """
-        Try to force something out of the ASIC
-        """
-        print("sending hard int..")
-        addr = QDA_CTRL_REG.SEND_DEBUG.value
-        self.eth.regWrite(addr, 1)
-
-
-    ############################
     ## Zybo specific Commands ##
     ############################
-    def readID(self):
-        """
-        Main function to send a trigger to update the two register addresses to read the eeprom ID
-        """
-        self.trigger(hard=False)
-        time.sleep(0.100)
-        lowVal = self.readReg(2)
-        data = 0xffff_ffff & lowVal
-        data = data >> 4
-        highVal = self.readReg(3)
-        highVal = highVal & 0xff
-        highVal = highVal << 28
-        data += highVal
-        print(f"reg read val: {data:08x}")
-
     def readReg(self, addr):
         """
         read a specific asic reg
@@ -292,39 +146,9 @@ class GUI(QMainWindow):
         print(f"writing val: 0x{val:04x}")
         self.eth.regWrite(addr, val)
 
-    def trigger(self, hard=True):
-        """
-        Send a basic trigger packet to the board.
-
-        This interrogation will be sent to all ASICs in the array, and memory
-        will be recorded into the BRAM within QpixDaqCtrl.vhd.
-        """
-        if hard:
-            print("sending trig")
-        addr = REG.TRIG
-        self.eth.regWrite(addr, 1)
-
-
-    def getDMARegisters(self):
-        """
-        Print the DMA register status, connected to a button
-        """
-        print("printing DMA Registers:")
-        self.eth.PrintDMA()
-
-
-    def resetDMA(self):
-        """
-        reset DMA by pinging the correct ctrl register
-        """
-        print("reseting the DMA!")
-        self.eth._resetDMA()
-
-
     ###########################
     ## GUI specific Commands ##
     ###########################
-
     def _make_menuBar(self):
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
@@ -351,9 +175,6 @@ class GUI(QMainWindow):
         """
         file save
         """
-        pass
-
-    def reject(self):
         pass
 
 
--- software/eth_interface.py
+++ software/eth_interface.py
@@ -1,100 +1,33 @@
 #!/usr/bin/env python
 
-import os
-import socket
 import struct
-import sys
-import glob
-from enum import Enum
-import time
 import datetime
 
 # Qt5 dependencies
-import PyQt5
-from PyQt5.QtCore import QObject, QByteArray, pyqtSignal, QThread, QEventLoop
+from PyQt5.QtCore import QObject, QByteArray, pyqtSignal, QThread
 from PyQt5.QtNetwork import QTcpSocket, QHostAddress, QUdpSocket
 
 # global defualts to configure connection to socket
-ETH_IP      = '192.169.1.27' # set local ethernet port to 192.169.1.17 to connect
-ETH_PORT    = 42069
+ETH_IP      = '192.168.137.16' # set local ethernet port to 192.169.1.17 to connect
+ETH_PORT    = 7
 BUFFER_SIZE = 1024
 
-# global of Zybo Frq which is used to calculate time value from timestamps
-ZYBO_FRQ = 30303030
-
 # UDP Info
-ETH_UDP_IP   = '192.169.1.17'
-ETH_UDP_PORT = 1337
+ETH_UDP_IP   = '192.168.137.16'
+ETH_UDP_PORT = 8
 EXIT_PACKET = bytes("ZaiJian", encoding="utf-8")
 PACKET_HEADER = bytes("HEADER", encoding="utf-8")
 DEFAULT_PACKET_SIZE = 1
 LATEST_STABLE_VERSION = 0x0000_00001
 
-# DMA_REG NOTES
-# CTRL NOTE: only lowest bit enables and begins DMA, bit 2 is always high, bit 16 is
-# interupt threshold which has a minimum of 1, and 1000 part is interrupt on complete enable
-# AXI DMA register info:  https://docs.xilinx.com/r/en-US/pg021_axi_dma/Product-Specification
-
-# DMA_CONTROL constant globals that should ensure DMA registers
-# each register stores 32b, but not all bits are used at certain registers..
-DMA_CTRL = 0x0001_1003
-DMA_STATUS = 0x0000_0000
-DMA_LENGTH = 0x0000_3fff
-DMA_DEST_MSB = 0x0000_0000
 
 class QDBBadAddr(Exception):
     pass
 
 
-class DMA_STATUS_BIT(Enum):
-    """
-    bits to be AND with of the status register, if the status register, which
-    should be all blank, returns a certain value.
-    """
-    HALT = (1 << 0)
-    IDLE = (1 << 1)
-    SGEN = (1 << 3)
-    INT_ERR = (1 << 4)
-    SLAV_ERR = (1 << 5)
-    DEC_ERR = (1 << 6)
-    SG_INT_ERR = (1 << 8)
-    SG_SLAV_ERR = (1 << 9)
-    SG_DEC_ERR = (1 << 10)
-    IOC_IRQ = (1 << 12)
-    DLY_IRQ = (1 << 13)
-    ERR_IRQ = (1 << 14)
-    IRQ_THRESH = (0x1 << 16)
-    IRQ_DELAY_STATUS = (0xff << 24)
-
-
 class EthBadAddr(Exception):
     pass
 
-
-class REG(Enum):
-    """
-    REG is an Enum class which returns a register address space for both the Zybo board
-    and all of the remote ASICs in an array.
-
-    There are two sets of addresses: Zybo address and remote ASIC address.
-
-    A Zybo address is treated as a normal Enum member, i.e. REG.CMD.value
-
-    A remote ASIC address is retrieved with REG.ASIC(xpos, ypos, AsicREG) method, i.e.
-    REG.ASIC(0, 0) returns the ASIC at x=0, y=0 position.
-
-    NOTE: There are two types of register transactions, either a read or a write; some
-    addresses only support one type.
-
-    specific register mappings are found in vhd files:
-    QpixProtoPkg.vhd
-    QpixRegFile.vhd
-    """
-
-    # all of these addresses are defined in QpixProtoPkg.vhd
-    # and are registers local to the Zybo/Aggregator
-    SCRATCH   = 0x00
-    TRIG = 0x01
 
 class UDPworker(QObject):
     """
@@ -161,25 +94,6 @@ class UDPworker(QObject):
             self.f = open(self.output_file, 'wb')
 
 
-class DMA_REG(Enum):
-    """
-    Enum class to specific DMA registers of interest to be used during
-    configuration at bootup.
-
-    This register space uses only the s2mm interface to control how the DMA
-    sends axi-stream data to the DDR memory of the Zybo.
-    """
-    S2MM_CTRL = 0x30
-    S2MM_STATUS = 0x34
-    S2MM_CURDESC = 0x38
-    S2MM_CURDESC_MSB = 0x3C
-    S2MM_TAILDESC = 0x40
-    S2MM_TAILDESC_MSB = 0x44
-    S2MM_DEST_ADDR = 0x48
-    S2MM_DEST_ADDR_MSB = 0x4C
-    S2MM_LENGTH = 0x58
-
-
 class eth_interface(QObject):
     """
     Generic interface class which manages the socket transactions and retrieves
@@ -201,7 +115,6 @@ class eth_interface(QObject):
         self._ETH_IP = QHostAddress(ETH_IP)
         self._ETH_PORT = port
         self.version = 0
-        # self._BUFFER_SIZE = BUFFER_SIZE
 
         # storage for retrieiving tcp data
         self.data = None
@@ -214,15 +127,7 @@ class eth_interface(QObject):
             self._tcpsocket.readyRead.connect(lambda: self._readData())
 
             # make sure to check this works
-            self._verify()
-
-            # begin the DMA configuration cycle
-            try:
-                self.PrintDMA()
-                self._dma_enabled = True
-            except Exception as ex:
-                print("unable to configure DMA engine")
-                self._dma_enabled = False
+            # self._verify()
 
         # manage the SAQ-UDP thread reader
         self.thread = QThread()
@@ -231,17 +136,13 @@ class eth_interface(QObject):
         self.thread.started.connect(self.worker.run)
         self.worker.finished.connect(self.udp_done)
 
-    def regRead(self, addr: REG) -> int:
-        """
-        read a Zybo register or a remote ASIC register as defined in REG class.
+    def isConnected(self) -> bool:
+        return self._tcpConnected
 
-        Returns last 32 bit word from the register readout.
+    def regRead(self, addr) -> int:
         """
-        # allow passing of REG enum types directly
-        if not isinstance(addr, REG) and hasattr(addr, "value"):
-            raise QDBBadAddr("Incorrect REG address on regRead!")
-        elif hasattr(addr, "value"):
-            addr = addr.value
+        read a particular address from the Zynq
+        """
 
         # form byte message
         args = ['QRR', addr]
@@ -266,18 +167,10 @@ class eth_interface(QObject):
 
     def regWrite(self, addr, val) -> int:
         """
-        Register write command, used for either writing directly to remote ASICs
-        or Zybo.
+        write to a particular address from the Zynq
         """
-        # allow passing of REG enum types directly
-        if not isinstance(addr, REG) and hasattr(addr, "value"):
-            raise EthBadAddr("Incorrect REG address on regWrite!")
-        elif hasattr(addr, "value"):
-            addr = addr.value
-        if hasattr(val, "value"):
-            val = val.value
+        print(f"writing {val:02x} to addr: {addr:06x}")
 
-        # print(f"writing {val:02x} to addr: {addr:06x}")
         # form byte message
         args = ['QRW', addr, val]
         if isinstance(args, str): args = args.split(' ')
@@ -296,29 +189,27 @@ class eth_interface(QObject):
         """
         initialization function to make sure that the interface can communicate
         with the scratch buffer.
-
-        A correct verification performs a successful regRead and regWrite of the
-        REG.SCRATCH buffer.
         """
-        self.version = self.regRead(REG.SCRATCH)
-        print(f"Running version: 0x{self.version:08x}.. verifying..", end=" ")
+        pass
+        # self.version = self.regRead(REG.SCRATCH)
+        # print(f"Running version: 0x{self.version:08x}.. verifying..", end=" ")
 
-        # update and check
-        checksum = 0x0a0a_a0a0
-        self.regWrite(REG.SCRATCH, checksum)
-        verify  = self.regRead(REG.SCRATCH)
-        if checksum != verify:
-            print("warning verification failed")
-            print(f"0x{checksum:08x} != 0x{verify:08x}")
-        else:
-            print("verification passed!")
-            self.regWrite(REG.SCRATCH, self.version)
+        # # update and check
+        # checksum = 0x0a0a_a0a0
+        # self.regWrite(REG.SCRATCH, checksum)
+        # verify  = self.regRead(REG.SCRATCH)
+        # if checksum != verify:
+        #     print("warning verification failed")
+        #     print(f"0x{checksum:08x} != 0x{verify:08x}")
+        # else:
+        #     print("verification passed!")
+        #     self.regWrite(REG.SCRATCH, self.version)
 
-        # set up SAQ register if version >= 8
-        if self.version <= LATEST_STABLE_VERSION:
-            print("WARNING NOT A STABLE VERSION")
+        # # set up SAQ register if version >= 8
+        # if self.version <= LATEST_STABLE_VERSION:
+        #     print("WARNING NOT A STABLE VERSION")
 
-        return checksum == verify
+        # return checksum == verify
 
     def _readData(self) -> int:
         """
@@ -362,117 +253,6 @@ class eth_interface(QObject):
             print("unconnected! TCP")
 
         return connected
-
-    def _WriteDMA(self, addr, val):
-        """
-        Write a DMA register, based on regWrite, but with special args header
-        """
-        # allow passing of REG enum types directly
-        if not isinstance(addr, DMA_REG) and hasattr(addr, "value"):
-            raise QDBBadAddr("Incorrect DMA_REG address on _WriteDMA!")
-        elif hasattr(addr, "value"):
-            addr = addr.value
-        if hasattr(val, "value"):
-            val = val.value
-
-        # form byte message
-        args = ['QDW', addr, val]
-        if isinstance(args, str): args = args.split(' ')
-        hdr = args[0]+'\0'
-        byte_arr = str.encode(hdr)
-        for arg in args[1:]:
-            if not isinstance(arg, int): arg = int(arg, 0)
-            byte_arr += struct.pack('<I', arg)
-
-        # returns number of bytes written
-        cnt = self._write(byte_arr)
-        self._tcpsocket.waitForReadyRead(1000)
-        return cnt
-
-    def _ReadDMA(self, addr):
-        """
-        Read a DMA register, based on regRead, but with special args header
-        """
-        # allow passing of REG enum types directly
-        if not isinstance(addr, DMA_REG) and hasattr(addr, "value"):
-            raise QDBBadAddr("Incorrect DMA_REG address on _ReadDMA!")
-        elif hasattr(addr, "value"):
-            addr = addr.value
-
-        args = ['QDR', addr]
-        if isinstance(args, str): args = args.split(' ')
-        hdr = args[0]+'\0'
-        byte_arr = str.encode(hdr)
-        for arg in args[1:]:
-            if not isinstance(arg, int): arg = int(arg, 0)
-            byte_arr += struct.pack('<I', arg)
-
-        self._write(byte_arr)
-        self._tcpsocket.waitForReadyRead(1000)
-
-        # make sure there's new data to return
-        if self.data is not None:
-            data = self.data
-            self.data = None
-            return data
-        else:
-            print('WARNING: no DMA REG data!')
-            return None
-
-    def PrintDMA(self):
-        """
-        interface should automagically configure and setup DMA registers, since
-        this usage uses a special register IO. this function should be used at
-        start up to ensure that the DMA registers can be configured properly,
-        and if not disable the DMA engine.
-        """
-
-        # initialize DMA ctrl on bootup if not running. This should be only register to ever write
-        addr = DMA_REG.S2MM_CTRL
-        d_ctrl = self._ReadDMA(addr)
-        if d_ctrl != DMA_CTRL:
-            print(f"WARNING! Initial DMA ctrl status: {d_ctrl:08x}")
-            print(f"EXPECTED: {DMA_CTRL:08x}")
-        self._WriteDMA(addr, DMA_CTRL)
-
-        # check DMA destination registers, should NOT write this! Embedded software
-        # and DMA control this register
-        addr = DMA_REG.S2MM_DEST_ADDR
-        d_dest = self._ReadDMA(addr)
-        if d_dest != DMA_DEST_MSB:
-            print(f"WARNING! DMA Destination Reg is: {d_dest:08x}")
-            print(f"EXPECTED: {DMA_DEST_MSB:08x}")
-
-        # update the current length buffer
-        addr = DMA_REG.S2MM_LENGTH
-        # self._WriteDMA(addr, DMA_LENGTH)
-        dma_leng = self._ReadDMA(addr)
-        if dma_leng != DMA_LENGTH:
-            print(f"WARNING! DMA Length Reg is: {dma_leng:08x}")
-            print(f"EXPECTED: {DMA_LENGTH:08x}")
-
-        # verify DMA status
-        addr = DMA_REG.S2MM_STATUS
-        dma_stat = self._ReadDMA(addr)
-        if dma_stat != DMA_STATUS:
-            print(f"WARNING DMA Status NOT as expected: {dma_stat:08x} NOT {DMA_STATUS:08x}")
-            for r in DMA_STATUS_BIT:
-                if r.value & dma_stat:
-                    print(f"REG {r} has error with DMA bit")
-
-        self._dma_enabled = True
-        return self._dma_enabled
-
-    def _resetDMA(self):
-        """
-        NOTE: Mostly deprecated! Embedded software should handle DMA resets
-        Write a bit to the second slot of the ctrl register and read back when it's zero..
-        """
-        # initialize DMA ctrl
-        addr = DMA_REG.S2MM_CTRL
-        self._WriteDMA(addr, DMA_CTRL+(1 << 2))
-        d_ctrl = self._ReadDMA(addr)
-        print(f"Final DMA ctrl status: {d_ctrl:08x}")
 
     def udp_done(self):
         """
